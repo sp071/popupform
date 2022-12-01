e88cf508e39b26a3b5a1f75f1933111d08d633f0
071Sabin <103927757+071Sabin@users.noreply.github.com>
2022-12-02 00:33:05 +0530

    Add files via upload

---
+++ popupForm/popupForm.js
@@ -0,0 +1,10 @@
+function openForm() {
+    document.getElementById("mainForm").style.display = "block";
+    document.getElementsByTagName('body').style.backgroundColor = "green";
+    // console.log("Opened!!");
+}
+
+function closeForm() {
+    document.getElementById("mainForm").style.display = "none";
+    // console.log("Closed!!");
+}
